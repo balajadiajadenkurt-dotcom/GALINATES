32c9631c36a6749db98924ae34e9ab420638037a
balajadiajadenkurt-dotcom <balajadia_jadenkurt@spcc.edu.ph>
2026-01-20 17:59:53 +0800

    sadas

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,20 +5,21 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float moveSpeed = 5f;
+    public float moveSpeed = 5f;
+
     private Rigidbody2D rb;
     private Vector2 moveInput;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-      rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        rb.linearVelocity = moveInput = moveSpeed;
+        rb.velocity = moveInput * moveSpeed;
     }
+
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
