55943704a7587b12bcc25e1d44f29083f1b9ca92
balajadiajadenkurt-dotcom <balajadia_jadenkurt@spcc.edu.ph>
2026-03-18 18:05:59 +0800

    nilalamig na ko im out

--- Assets/QuizManager.cs
+++ Assets/QuizManager.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
+using TMPro;
+using System.Collections;
 
 public class QuizManager : MonoBehaviour
 {
+    [Header("UI")]
     public GameObject questionPanel;
     public TextMeshProUGUI questionText;
     public Button[] answerButtons;
+    public TextMeshProUGUI resultText;
+
+    [Header("Questions")]
     public Question[] questions;
 
     int currentQuestion;
+    int score = 0;
 
+    // 🔓 Called when pressing E near trashcan
     public void OpenQuestion()
     {
         questionPanel.SetActive(true);
+        resultText.gameObject.SetActive(false);
         ShowQuestion();
     }
 
@@ -28,24 +36,48 @@ public class QuizManager : MonoBehaviour
         {
             int index = i;
 
+            // Set answer text
             answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = q.answers[i];
 
+            // Reset color
+            answerButtons[i].GetComponent<Image>().color = Color.white;
+
+            // Remove old listeners
             answerButtons[i].onClick.RemoveAllListeners();
+
+            // Add new listener
             answerButtons[i].onClick.AddListener(() => CheckAnswer(index));
         }
     }
 
-    void CheckAnswer(int index)
+    public void CheckAnswer(int index)
     {
-        if (index == questions[currentQuestion].correctAnswer)
+        bool isCorrect = index == questions[currentQuestion].correctAnswer;
+
+        if (isCorrect)
         {
-            Debug.Log("Correct!");
+            score += 10;
+            resultText.text = "Correct!";
+            answerButtons[index].GetComponent<Image>().color = Color.green;
         }
         else
         {
-            Debug.Log("Wrong!");
+            resultText.text = "Wrong!";
+            answerButtons[index].GetComponent<Image>().color = Color.red;
+
+            // Highlight correct answer
+            int correct = questions[currentQuestion].correctAnswer;
+            answerButtons[correct].GetComponent<Image>().color = Color.green;
         }
 
+        resultText.gameObject.SetActive(true);
+
+        StartCoroutine(CloseAfterDelay());
+    }
+
+    IEnumerator CloseAfterDelay()
+    {
+        yield return new WaitForSeconds(1.5f);
         questionPanel.SetActive(false);
     }
 }
